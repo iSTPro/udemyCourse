25ee489d7184edef7e5e231baca327cbc7114002
unknown <educarl.set@gmail.com>
2023-04-08 13:43:42 +0300

    fixed some line code to match the Tutorial

--- for_loop.cpp
+++ for_loop.cpp
@@ -5,7 +5,7 @@ int main()
 {
     //print out I love C++ SEVEN HUNDRES TIME USING for loop
 
-    for(unsigned int i{0}; i < 10; i++){
+    for(unsigned int i{0}; i < 100; i++){
         cout<<"I Love C++"<<endl;
     }
     return 0;
