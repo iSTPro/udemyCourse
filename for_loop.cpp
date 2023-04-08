99be581855686cf65222037a02fdee558ef08182
unknown <educarl.set@gmail.com>
2023-04-08 13:40:04 +0300

    first push

---
+++ for_loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //print out I love C++ SEVEN HUNDRES TIME USING for loop
+
+    for(unsigned int i{0}; i < 10; i++){
+        cout<<"I Love C++"<<endl;
+    }
+    return 0;
+}
